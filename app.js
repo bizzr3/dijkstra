128110d32f0373d90156c76b4705861c4a566f78
bizzr3 <mojtaba.bakhtiaree@gmail.com>
2018-10-08 03:57:10 +0700

    removed favicon lines

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
-const favicon = require('serve-favicon')
 const commandParser = require('./src/utils/commandParser/commandParser')
 
 const cmdParser = new commandParser();
@@ -14,7 +13,6 @@ const cmdParser = new commandParser();
 app.use('/public', express.static(process.env.PWD + '/public'));
 app.use('/jquery', express.static(process.env.PWD + '/node_modules/jquery/dist/'));
 app.use('/bootstrap', express.static(process.env.PWD + '/node_modules/bootstrap/dist/'));
-app.use(favicon(path.join(process.env.PWD + '/public/assets/img/Eko-logo.ico')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(process.env.PWD + '/public/views/index.html'));
