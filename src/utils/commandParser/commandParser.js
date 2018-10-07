1cae481be01345fd2077ae276b9955e4e0900f2b
bizzr3 <mojtaba.bakhtiaree@gmail.com>
2018-10-07 22:52:06 +0700

    add route command and add validation for it.

--- src/utils/commandParser/commandParser.js
+++ src/utils/commandParser/commandParser.js
@@ -1,24 +1,40 @@
 'use strict';
 const PropertiesReader = require('properties-reader');
 const replies = PropertiesReader(process.env.PWD + '/src/utils/commandParser/replies.properties')
+const Router = require
 
 class commandParser {
     constructor() {
-        this.supportedCmds = [
-            'route'
-        ]
+        this.supportedCmds = {
+            route: {
+                max_segments: 4,
+                max_args: 2,
+                options: ['-all', '-c'],
+            }
+        }
 
         this.errors = {
-            WSH_CMD_NOT_FOUND: 404
+            WSH_CMD_NOT_FOUND: 404,
+            WSH_CMD_BAD_REQUEST: 400,
+            WSH_CMD_NOT_ENOUGH_ARGS: 309
         }
     }
 
     parseCmd(cmd) {
-        cmd = cmd.split(' ');
 
-        if (!this.isCmdExists(cmd[0])) {
-            return this.generateReply(this.errors.WSH_CMD_NOT_FOUND, cmd[0])
+        cmd = cmd.replace(/(\r\n|\n|\r)/gm, "")
+            .trim()
+            .split(' ');
+
+        //TODO: add CMD name to the class properties.
+
+        let isInvalidCmd = this.validateCmd(cmd);
+
+        if (isInvalidCmd) {
+            return isInvalidCmd;
         }
+
+        return 'CMD ok.'
     }
 
     generateReply(type, data) {
@@ -26,11 +42,44 @@ class commandParser {
             case this.errors.WSH_CMD_NOT_FOUND:
                 return `<span class='text-danger'>wsh: ${replies.get('not-found')}: ${data}</span>`;
                 break;
+            case this.errors.WSH_CMD_BAD_REQUEST:
+                return `${replies.get('invalid-cmd')}`;
+                break;
         }
     }
 
+    validateCmd(cmd) {
+        if (!this.isCmdExists(cmd[0])) {
+            return this.generateReply(this.errors.WSH_CMD_NOT_FOUND, cmd[0])
+        }
+
+        if (!this.hasValidSegments(cmd[0], cmd.length)) {
+            return this.generateReply(this.errors.WSH_CMD_BAD_REQUEST, cmd[0])
+        }
+
+        if (!this.hasValidOption(cmd[0], cmd[1])) {
+            return this.generateReply(this.errors.WSH_CMD_BAD_REQUEST, cmd[0])
+        }
+
+        return false;
+    }
+
     isCmdExists(cmd) {
-        return this.supportedCmds[cmd] ? true : false
+        return this.supportedCmds[cmd] ? true : false;
+    }
+
+    hasValidOption(cmd, option) {
+        for (let index in this.supportedCmds[cmd].options) {
+            if (this.supportedCmds[cmd].options[index] === option) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    hasValidSegments(cmd, segments) {
+        return this.supportedCmds[cmd].max_segments == segments ? true : false;
     }
 }
 
